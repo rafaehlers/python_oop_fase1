02a3ed2887d62ad43b41af7eaf4c7eaa7572a985
Rafael Ehlers <rafaehlers@gmail.com>
2024-08-18 22:52:51 -0300

    Adiciona lógica de CSV
    define módulo e cria método de leitura de CSV já com tratamento de exceções

--- steam_data/data_loader.py
+++ steam_data/data_loader.py
@@ -1,1 +1,20 @@
 import csv
+from steam_data.exceptions import DataLoadException
+
+class DataLoader:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.games = []
+
+    def load_data(self):
+        try:
+            with open(self.file_path, mode='r', encoding='utf-8') as file:
+                reader = csv.DictReader(file)
+                for row in reader:
+                    self.games.append(row)
+        except FileNotFoundError:
+            raise DataLoadException(f"Arquivo {self.file_path} não encontrado.")
+        except Exception as e:
+            raise DataLoadException(f"Erro ao carregar os dados: {e}")
+
+        return self.games
