d49e4bcdd55bed467b86391f6e318a038bf9359c
Rafael Ehlers <rafaehlers@gmail.com>
2024-08-18 23:11:05 -0300

    Responde primeira pergunta
    Adiciona método que responde a primeira pergunta e printa na tela o resultado.
    
    fixes #9

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from steam_data.data_loader import DataLoader
+from steam_data.game_statistics import GameStatistics
 
 def main():
     loader = DataLoader('steam_games.csv')
     games = loader.load_data()
+
+    stats = GameStatistics(games)
+    
+    # Responder perguntas
+    print("Percentual de jogos gratuitos e pagos:")
+    print(stats.percentual_jogos_gratuitos_pagos())
+
+if __name__ == "__main__":
+    main()
--- steam_data/game_statistics.py
+++ steam_data/game_statistics.py
@@ -3,3 +3,12 @@ from collections import defaultdict
 class GameStatistics:
     def __init__(self, games):
         self.games = games
+
+    def percentual_jogos_gratuitos_pagos(self):
+        gratuitos = sum(1 for game in self.games if float(game['Price']) == 0)
+        pagos = len(self.games) - gratuitos
+        total = len(self.games)
+        return {
+            "gratuitos": (gratuitos / total) * 100,
+            "pagos": (pagos / total) * 100
+        }
