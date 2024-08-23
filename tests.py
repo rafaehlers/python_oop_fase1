a2d7f6edfd8f8551cb8211fe44281a5b0e93daba
Rafael Ehlers <rafaehlers@gmail.com>
2024-08-22 23:09:50 -0300

    Muda teste e coluna do score
    Acrescentado teste para a terceira pergunta e coluna para calculo de score foi modificada.

--- tests.py
+++ tests.py
@@ -18,11 +18,41 @@ class TestSteamData(unittest.TestCase):
 
     def test_percentual_jogos_gratuitos_pagos(self):
         resultado = self.stats.percentual_jogos_gratuitos_pagos()
-        self.assertEqual(resultado, "Gratuitos: 20.00%, Pagos: 80.00%")
+        self.assertEqual(resultado, "Gratuitos: 15.00%, Pagos: 85.00%")
 
     def test_ano_com_mais_jogos(self):
         anos_max = self.stats.ano_com_mais_jogos()
-        self.assertIn("2022", anos_max)
+        self.assertIn("2021", anos_max)
+
+    def test_jogo_com_maior_nota_por_categoria(self):
+        output_esperado = (
+            "Single-player                  | DCS World Steam Edition                  | 19580.0\n"
+            "Multi-player                   | DCS World Steam Edition                  | 19580.0\n"
+            "PvP                            | DCS World Steam Edition                  | 19580.0\n"
+            "Online PvP                     | DCS World Steam Edition                  | 19580.0\n"
+            "LAN PvP                        | DCS World Steam Edition                  | 19580.0\n"
+            "Co-op                          | DCS World Steam Edition                  | 19580.0\n"
+            "Online Co-op                   | DCS World Steam Edition                  | 19580.0\n"
+            "LAN Co-op                      | DCS World Steam Edition                  | 19580.0\n"
+            "Captions available             | DCS World Steam Edition                  | 19580.0\n"
+            "VR Support                     | DCS World Steam Edition                  | 19580.0\n"
+            "Partial Controller Support     | DCS World Steam Edition                  | 19580.0\n"
+            "Includes level editor          | DCS World Steam Edition                  | 19580.0\n"
+            "Steam Leaderboards             | Modbox                                   | 191.0\n"
+            "Steam Achievements             | Tormented Souls                          | 3229.0\n"
+            "Steam Trading Cards            | Shot In The Dark                         | 78.0\n"
+            "Shared/Split Screen PvP        | Modbox                                   | 191.0\n"
+            "Steam Workshop                 | Modbox                                   | 191.0\n"
+            "Stats                          | Virtual Army: Revolution                 | 13.0\n"
+            "Remote Play Together           | Modbox                                   | 191.0\n"
+            "Steam Cloud                    | That Time I Got Reincarnated as an Orc   | 243.0\n"
+            "Full controller support        | Modbox                                   | 191.0\n"
+            "Shared/Split Screen Co-op      | Modbox                                   | 191.0\n"
+            "Shared/Split Screen            | Modbox                                   | 191.0\n"
+            "Cross-Platform Multiplayer     | Modbox                                   | 191.0"
+        )
+        resultado = self.stats.jogo_com_maior_nota_por_categoria()
+        self.assertEqual(resultado, output_esperado)
 
 if __name__ == "__main__":
     unittest.main()
